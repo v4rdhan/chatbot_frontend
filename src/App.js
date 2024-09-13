9e4cce3297fa5c980002b5cb0d3df95363e70389
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2024-09-14 01:40:45 +0530

    added Hashrouting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from "./logo.svg";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 // import PrivateRoute from "./PrivateRoute";
 import "./App.css";
 import Login from "./login/Login";
@@ -9,14 +9,14 @@ import Chatbot from "./chatbot/Chatbot";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<Login />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="chatbot" element={<Chatbot />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chatbot" element={<Chatbot />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
